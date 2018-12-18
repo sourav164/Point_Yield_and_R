19afac6f7ba1997b0a06477cae259ad4e0766245
sourav164 <sourav.abe@gmail.com>
2018-12-18 11:44:57 -0600

    Update NDVI_Average.py

--- NDVI_Average.py
+++ NDVI_Average.py
@@ -1,3 +1,4 @@
+#import necessary libraries
 import os, fnmatch, arcpy, numpy, pandas as pd
 import numpy as np
 from arcpy.sa import *
@@ -6,15 +7,16 @@ from arcpy.sa import *
 l_list = []
 
 for path in l_list:
-
     i, j = os.path.split(path)
     print (j)
     
     satellite = {"*Intel*": ["IntelinAir - 0.16m","ko"], "*PSC*":["PlanetScope - 3m","gv"],  "*Sent*":["Sentinal - 10m", "mp"],  "*RE*":["Rapid-Eye - 5m", "rd"]}
+    #create a excel file to write
     point_data = os.path.join(path, str(j+"_average.xls"))
     writer = pd.ExcelWriter(point_data)
+    
+    #one satelite at a time
     for item in satellite:
-        
         df1 = pd.DataFrame()
         df1["Date"] = ["Mean",    "5 Percentile"   , "95 Percentile" ,  "Standard Deviation", "SAT" ]
         
@@ -22,6 +24,7 @@ for path in l_list:
         for root, dirs, files in os.walk(path):
                 for file in files:
                         if fnmatch.fnmatch(file, item) and file.endswith("V1NDVI.tif"):
+                                #convert the raster into a numpy array then get properties
                                 original = os.path.join(root, file)
                                 array = arcpy.RasterToNumPyArray(Raster(original),nodata_to_value=9999)
                                 marray = np.ma.masked_values(array,9999)
@@ -32,6 +35,7 @@ for path in l_list:
                                 sat_name =item.replace("*", "")
                                 
 
+                                #write data to the file
                                 da = file[-19:-11]
                                 date =str(da[0:4]+"/"+da[4:6]+"/"+da[6:])
                                 df1[date] = [mean, five, ninefive, std, sat_name]
